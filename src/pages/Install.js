fd3a76e6f5d869f0d2b0a1a227fa3c33713bcbbb
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-08 12:03:19 +0100

    Fix use.js

--- src/pages/Install.js
+++ src/pages/Install.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Install = () => {
     return (
         <div>
             <h2>Comment l'installer ?</h2>
-            <p>Pour installer React Router, il suffit de faire comme pour tous les autres packages, c'est à dire, en utilisant le terminal.</p>
-            <img src={`${process.env.PUBLIC_URL}/img/npm.png`}  alt="" />
+            <p>
+                Pour installer React Router, il suffit de faire comme pour tous
+                les autres packages, c'est-à-dire, en utilisant le terminal.
+            </p>
+            <img src={`${process.env.PUBLIC_URL}/img/npm.png`} alt="" />
         </div>
     );
 };
--- src/pages/Use.js
+++ src/pages/Use.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Use = () => {
     return (
@@ -11,28 +11,37 @@ const Use = () => {
                 <li>Importer BrowserRouter</li>
                 <li>Entourer la balise App avec BrowserRouter</li>
             </ul>
-            <img src={`${process.env.PUBLIC_URL}/img/index.png`}  alt="" />
+            <img src={`${process.env.PUBLIC_URL}/img/index.png`} alt="" />
 
             <h3>Etape 2 :</h3>
             <p>Créer plusieurs pages et les routes dans App.js.</p>
-            <p>Par exemple ce site contient 3 pages Home.js, Install.js et Use.js.</p>
+            <p>
+                Par exemple, ce site contient 3 pages Home.js, Install.js et
+                Use.js.
+            </p>
             <ul>
                 <li>Importer Routes et Route</li>
-                <li>Créer des Route dans Routes en respectant path et element</li>
+                <li>
+                    Créer des Route dans Routes en respectant path et element
+                </li>
             </ul>
-            <img src={`${process.env.PUBLIC_URL}/img/app.png`}  alt="" />
+            <img src={`${process.env.PUBLIC_URL}/img/app.png`} alt="" />
 
             <h3>Etape 3 :</h3>
-            <p>Créer votre Navbar.js (vous pouvez metter n'importe quel nom).</p>
+            <p>
+                Créer votre Navbar.js (vous pouvez mettre n'importe quel nom).
+            </p>
             <ul>
                 <li>Importer Link</li>
-                <li>Créer une balise nav avec des Link en spécifiant le to=" "</li>
+                <li>
+                    Créer une balise nav avec des Link en spécifiant le to=" "
+                </li>
             </ul>
-            <img src={`${process.env.PUBLIC_URL}/img/navbar.png`}  alt="" />
+            <img src={`${process.env.PUBLIC_URL}/img/navbar.png`} alt="" />
 
             <h3>Etape 4 :</h3>
             <p>Ajouter la Navbar.js dans App.js.</p>
-            <img src={`${process.env.PUBLIC_URL}/img/addnav.png`}  alt="" />
+            <img src={`${process.env.PUBLIC_URL}/img/addnav.png`} alt="" />
         </div>
     );
 };
