446d1754f229ac163db8a8eaa5655fbfcd2103e6
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-08 12:17:33 +0100

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
     return (
         <div>
             <h2>C'est quoi ?</h2>
-            <p>React Router est une bibliothèque standard pour le routage dans React. Elle permet de naviguer entre les vues de divers composants dans une application React, de modifier l'URL du navigateur et de synchroniser l'interface utilisateur avec l'URL.</p>
-            <img src={`${process.env.PUBLIC_URL}/img/code.png`}  alt="" />
+            <p>
+                React Router est une bibliothèque standard pour le routage dans
+                React. Elle permet de naviguer entre les vues de divers
+                composants dans une application React, de modifier l'URL du
+                navigateur et de synchroniser l'interface utilisateur avec
+                l'URL.
+            </p>
+            <img src={`${process.env.PUBLIC_URL}/img/code.png`} alt="" />
         </div>
     );
 };
--- src/pages/Install.js
+++ src/pages/Install.js
@@ -6,7 +6,7 @@ const Install = () => {
             <h2>Comment l'installer ?</h2>
             <p>
                 Pour installer React Router, il suffit de faire comme pour tous
-                les autres packages, c'est-à-dire, en utilisant le terminal.
+                les autres packages, c'est à dire, en utilisant le terminal.
             </p>
             <img src={`${process.env.PUBLIC_URL}/img/npm.png`} alt="" />
         </div>
